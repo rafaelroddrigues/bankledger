8a78ba39967673e15691ab46989b495245b82c2c
Rafael Rodrigues <rafaelroddrigues@gmail.com>
2025-04-27 23:30:37 -0300

    Remove amount attribute from Account model

--- src/main/java/com/bankledger/model/Account.java
+++ src/main/java/com/bankledger/model/Account.java
@@ -1,16 +1,16 @@
 package com.bankledger.model;
 
-public record Account(String accountNumber, double amount, double balance) {
+public record Account(String accountNumber, double balance) {
 
     public Account(String accountNumber) {
-        this(accountNumber, 0, 0);
+        this(accountNumber, 0);
     }
 
     public Account deposit(double amount) {
-        return new Account(this.accountNumber, this.amount, this.balance + amount);
+        return new Account(this.accountNumber, this.balance + amount);
     }
 
     public Account withdraw(double amount) {
-        return new Account(this.accountNumber, this.amount, this.balance - amount);
+        return new Account(this.accountNumber, this.balance - amount);
     }
 }
